1421db917b33367c1c7ecf0fc15afa7d6ea4f546
Tobi <steltner@pm.me>
2025-07-15 00:55:35 +0200

    modified:   astro.config.mjs 	modified:   package.json 	deleted:    vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
-const build = {
-  baseURL: '/',
-  outDir: './dist',
-  siteURL: 'https://www.steltner.cc/'
-}
-
-const env = process.env.NODE_ENV
-
-switch (env) {
-  case 'pages':
-    build.baseURL = '/astro-landing-page/'
-    build.outDir = './docs'
-    build.siteURL = 'https://ttntm.github.io/astro-landing-page/'
-    break
-
-  case 'production':
-    build.siteURL = 'https://www.steltner.cc/'
-    break
-
-  default:
-    break
-}
-
 // https://astro.build/config
 export default defineConfig({
-  site: build.siteURL,
-  base: build.baseURL,
-  outDir: build.outDir,
-  output: 'static', // Generate static site
+  site: 'https://www.steltner.cc/',
+  base: '/',
+  outDir: './dist',
+  output: 'static',
   integrations: [
     tailwind({
       applyBaseStyles: false
